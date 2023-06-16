9c29f286aa4c10adf5b391fcc0aae62a983cbbb9
Dmytro <ihnatenko.dev@gmail.com>
2023-06-16 12:16:38 +0300

    Update auth model

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,15 @@
-export class AuthModel {
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ versionKey: false, collection: 'users' })
+export class UserModel {
+  @Prop({ unique: true })
   email: string;
+
+  @Prop()
   passwordHash: string;
+
+  @Prop()
+  name: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(UserModel);
