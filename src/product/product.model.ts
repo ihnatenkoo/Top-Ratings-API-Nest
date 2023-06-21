721b578f11604142c3337bcd0410ae74f28cc268
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-06-21 20:28:39 +0300

    Fixed product model

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,16 +1,46 @@
-export class ProductModel {
-  _id: string;
-  image: string;
-  title: string;
-  price: number;
-  oldPrice: number;
-  credit: number;
-  calculatedRating: number;
-  description: string;
-  advantages: string;
-  disadvantages: string;
-  categories: string[];
-  characteristics: {
-    [key: string]: string;
-  };
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+class ProductCharacteristic {
+  name: string;
+  value: string;
 }
+@Schema({ versionKey: false, collection: 'products' })
+export class ProductModel {
+  @Prop()
+  image: string;
+
+  @Prop()
+  title: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  oldPrice?: number;
+
+  @Prop()
+  credit: number;
+
+  @Prop()
+  calculatedRating: number;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  advantages: string;
+
+  @Prop()
+  disadvantages: string;
+
+  @Prop()
+  categories: string[];
+
+  @Prop()
+  tags: string[];
+
+  @Prop()
+  characteristics: ProductCharacteristic[];
+}
+
+export const ProductSchema = SchemaFactory.createForClass(ProductModel);
