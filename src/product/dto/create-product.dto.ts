f230f819a915b9fb91e01fa44afe2a8784fd691a
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-06-21 20:08:01 +0300

    Add validation to create-product dto

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,57 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+  ValidateNested,
+} from 'class-validator';
+
+class ProductCharacteristicDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  value: string;
+}
+
+export class CreateProductDto {
+  @IsUrl()
+  image: string;
+
+  @IsString()
+  title: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsOptional()
+  @IsNumber()
+  oldPrice?: number;
+
+  @IsNumber()
+  credit: number;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  advantages: string;
+
+  @IsString()
+  disadvantages: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  categories: string[];
+
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => ProductCharacteristicDto)
+  characteristics: ProductCharacteristicDto[];
+}
