b91c8050066b164c00a118b01722bb43b658fe64
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-06-23 18:21:00 +0300

    Create page dto

---
+++ src/top-page/dto/create-page.dto.ts
@@ -0,0 +1,64 @@
+import {
+  IsArray,
+  IsEnum,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { TopLevelCategory } from '../top-page.model';
+import { Type } from 'class-transformer';
+
+class VacanciesDto {
+  @IsNumber()
+  count: number;
+  @IsNumber()
+  juniorSalary: number;
+  @IsNumber()
+  middleSalary: number;
+  @IsNumber()
+  seniorSalary: number;
+}
+
+class AdvantagesDto {
+  @IsString()
+  title: string;
+  @IsString()
+  description: string;
+}
+
+export class CreatePageDto {
+  @IsEnum(TopLevelCategory)
+  firstCategory: string;
+
+  @IsString()
+  secondCategory: string;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  category: string;
+
+  @IsOptional()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => VacanciesDto)
+  vacancies: VacanciesDto;
+
+  @IsObject()
+  @ValidateNested()
+  @Type(() => AdvantagesDto)
+  advantages: AdvantagesDto;
+
+  @IsString()
+  seoText: string;
+
+  @IsString()
+  tagsTitle: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+}
