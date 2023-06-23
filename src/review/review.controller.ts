725250680b10f3dbdc657af1c56a98fbc6f41759
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-06-23 14:01:35 +0300

    Add id validatuion pipe to review controller

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -10,12 +10,12 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-
 import { User } from 'src/decorators/user.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { CreateReviewDto } from './dto/createReviewDto';
 import { ReviewService } from './review.service';
 import { ReviewModel } from './review.model';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 
 @Controller('review')
 export class ReviewController {
@@ -25,8 +25,8 @@ export class ReviewController {
   @UseGuards(JwtAuthGuard)
   @UsePipes(new ValidationPipe())
   async create(
-    @User('id') userId: string,
-    @Query('product') productId: string,
+    @User('id', IdValidationPipe) userId: string,
+    @Query('product', IdValidationPipe) productId: string,
     @Body() createReviewDto: CreateReviewDto,
   ): Promise<ReviewModel> {
     return await this.reviewService.create(userId, productId, createReviewDto);
@@ -35,16 +35,16 @@ export class ReviewController {
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
   async delete(
-    @User('id') userId: string,
-    @Query('product') productId: string,
-    @Param('id') reviewId: string,
+    @User('id', IdValidationPipe) userId: string,
+    @Query('product', IdValidationPipe) productId: string,
+    @Param('id', IdValidationPipe) reviewId: string,
   ): Promise<void> {
     await this.reviewService.delete(userId, productId, reviewId);
   }
 
   @Get('byProduct/:productId')
   async getByProduct(
-    @Param('productId') productId: string,
+    @Param('productId', IdValidationPipe) productId: string,
   ): Promise<ReviewModel[]> {
     return await this.reviewService.getByProduct(productId);
   }
@@ -52,7 +52,7 @@ export class ReviewController {
   @Delete('byProduct/:productId')
   @UseGuards(JwtAuthGuard)
   async deleteAllByProduct(
-    @Param('productId') productId: string,
+    @Param('productId', IdValidationPipe) productId: string,
   ): Promise<{ deletedCount: number }> {
     return await this.reviewService.deleteAllByProduct(productId);
   }
