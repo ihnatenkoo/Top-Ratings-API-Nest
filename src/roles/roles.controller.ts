e349dd0ddd6f5f71a641c2040cf65c526f8d32fe
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-06-30 16:41:08 +0300

    Implement getRoles [GET] api/roles, handle errors in create role

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,12 +1,14 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { RolesService } from './roles.service';
+import { RoleModel } from './role.model';
 
 @Controller('roles')
 export class RolesController {
@@ -14,7 +16,12 @@ export class RolesController {
 
   @UsePipes(new ValidationPipe())
   @Post()
-  async create(@Body() dto: CreateRoleDto) {
+  async create(@Body() dto: CreateRoleDto): Promise<RoleModel> {
     return await this.rolesService.create(dto);
+  }
+
+  @Get()
+  async getRoles(): Promise<RoleModel[]> {
+    return await this.rolesService.getRoles();
   }
 }
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { RoleModel } from './role.model';
 import { CreateRoleDto } from './dto/create-role.dto';
-import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class RolesService {
@@ -10,7 +10,17 @@ export class RolesService {
     @InjectModel(RoleModel.name) private readonly roleModel: Model<RoleModel>,
   ) {}
 
-  async create(dto: CreateRoleDto) {
+  async create(dto: CreateRoleDto): Promise<RoleModel> {
+    const roles = await this.getRoles();
+
+    if (roles.some((item) => item.role === dto.role)) {
+      throw new HttpException('Role already exists', HttpStatus.BAD_REQUEST);
+    }
+
     return this.roleModel.create(dto);
+  }
+
+  async getRoles(): Promise<RoleModel[]> {
+    return await this.roleModel.find();
   }
 }
